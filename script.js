9e11149ab11ace6bb45c36f649711937e64ce858
Kevin Fallas <48746116+KevinFallas03@users.noreply.github.com>
2023-06-30 19:08:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ var shockButton = document.getElementById('shockButton');
 var food = document.getElementById('food');
 var rat = document.getElementById('rat');
 var box = document.querySelector('.box');
+var light = document.getElementById('light');
 
 var foodCount = 0;
+var foodShownCount = 0;
+var lightCount = 0;
+var foodClickCount = 0; // Agregamos una variable para contar los clics en el botón de comida
 
-var foodEmojis = ['&#129385;', '&#129472;', '&#129372;', '&#129472;', '&#129360;', '&#129472;', '&#127829;', '&#129472;']; // Lista de emojis de comida
+var foodEmojis = ['&#129385;', '&#129472;', '&#129372;', '&#129472;', '&#129360;', '&#129472;', '&#127829;', '&#129472;'];
 
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
@@ -33,15 +37,20 @@ foodButton.addEventListener('click', function() {
   foodCount++;
 
   if (foodCount === 4) {
-    var randomIndex = Math.floor(Math.random() * foodEmojis.length);
-    var randomFoodEmoji = foodEmojis[randomIndex];
-    food.innerHTML = randomFoodEmoji;
-    var position = getRandomPosition();
-    food.style.top = position.y + 'px';
-    food.style.left = position.x + 'px';
-    food.style.display = 'block';
     foodCount = 0;
+    foodShownCount++;
+    if (foodShownCount <= 2) {
+      var randomIndex = Math.floor(Math.random() * foodEmojis.length);
+      var randomFoodEmoji = foodEmojis[randomIndex];
+      food.innerHTML = randomFoodEmoji;
+      var position = getRandomPosition();
+      food.style.top = position.y + 'px';
+      food.style.left = position.x + 'px';
+      food.style.display = 'block';
+    }
   }
+
+  foodClickCount++; // Incrementamos la variable de conteo de clics en el botón de comida
 });
 
 shockButton.addEventListener('click', function() {
@@ -50,4 +59,31 @@ shockButton.addEventListener('click', function() {
     document.body.style.backgroundColor = '';
   }, 3000);
   foodCount = 0;
+});
+
+setInterval(function() {
+  if (foodClickCount >= 8) { // Verificamos si se han hecho al menos 8 clics en el botón de comida
+    lightCount = 0;
+    light.style.display = 'block'; // Muestra la luz
+    light.style.backgroundColor = 'yellow';
+    setTimeout(function() {
+      light.style.backgroundColor = '';
+      light.style.display = 'none'; // Oculta la luz
+    }, 5000);
+  }
+}, 10000);
+
+foodButton.addEventListener('click', function() {
+  if (light.style.backgroundColor === 'yellow') {
+    lightCount++;
+    if (lightCount === 4) {
+      var randomIndex = Math.floor(Math.random() * foodEmojis.length);
+      var randomFoodEmoji = foodEmojis[randomIndex];
+      food.innerHTML = randomFoodEmoji;
+      var position = getRandomPosition();
+      food.style.top = position.y + 'px';
+      food.style.left = position.x + 'px';
+      food.style.display = 'block';
+    }
+  }
 });
